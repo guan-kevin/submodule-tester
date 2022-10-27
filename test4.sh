d825bb9a8e019969cea23b306b777658c19f9da3
Kevin Guan <20823498+guan-kevin@users.noreply.github.com>
2022-10-27 15:38:56 -0700

    Update test4.sh

--- test4.sh
+++ test4.sh
@@ -1,4 +1,4 @@
-if [[ $LARGER_THAN_0 == 1 && $HELLO == "world" ]]; then
+if [[ $LARGER_THAN_0 == 1 && $HELLO == "reproducer" ]]; then
 	echo "Environment variable in test.sh OK"
 else
 	echo "Bad environment variable in test.sh"
